ebae79557925b75e31514f5d1fc160ba84007467
Santiago Tellez <147094912+neutronwe@users.noreply.github.com>
2024-05-15 01:32:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.practicapoocasino;
 
 import java.util.Scanner;
@@ -13,31 +12,43 @@ public class Main {
         JuegoAhorcado juegoAhorcado = new JuegoAhorcado();
 
         while (true) {
-            System.out.println("--------INICIO CASINO--------");
+            System.out.println("                                                     ");
+            System.out.println(" -------------------INICIO CASINO------------------- ");
+            System.out.println("|                                                   |");
             
             int aux = juegoAdivinarNumero.getSaldo();
             int aux2 = juegoAhorcado.getSaldo();
             
             if (aux == 0 || aux2 == 0){
                 
-                System.out.print("Ingresa el saldo para jugar: ");
-          
+                System.out.print("| INGRESA EL SALDO PARA JUGAR: ");
                 juegoAdivinarNumero.setSaldo(scanner.nextInt());
                 juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
+                System.out.println(" --------------------------------------------------- ");  
+                
+                while (juegoAdivinarNumero.getSaldo() == 0){
+                    
+                    System.out.println("                                                     ");
+                    System.out.println(" --------------------------------------------------- ");  
+                    System.out.println("|                                                   |");
+                    System.out.println("| EL SALDO DEBE SER MAYOR A 0!!!!                   |");
+                    System.out.print  ("| INGRESE DE NUEVO EL SALDO: ");
+                    juegoAdivinarNumero.setSaldo(scanner.nextInt());
+                    juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
+                    System.out.println(" --------------------------------------------------- ");
+                }
             }
             
-            
-            System.out.println("                                 ");
-            System.out.println("SU SALDO ES: " + juegoAdivinarNumero.getSaldo());
-            System.out.println("                                          ");
-            System.out.println("1. Juego Adivina el número");
-            System.out.println("2. Juego El Ahorcado");
-            System.out.println("3. Salir");
-            System.out.println("                                   ");
-            System.out.print("Digite el numero de la opcion: ");
-            System.out.println("                                 ");
-
+            System.out.println("|                                                   |");
+            System.out.println("| SU SALDO ES: " + juegoAdivinarNumero.getSaldo());
+            System.out.println("|                                                   |");
+            System.out.println("| 1. Juego Adivina el numero                        |");
+            System.out.println("| 2. Juego El Ahorcado                              |");
+            System.out.println("| 3. Salir                                          |");
+            System.out.println("|                                                   |");
+            System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
             int opcion = scanner.nextInt();
+            System.out.println(" --------------------------------------------------- ");
             scanner.nextLine(); // Consumir la nueva línea después de nextInt()
             
             
@@ -46,24 +57,98 @@ public class Main {
                 case 1:
                     
                     juegoAdivinarNumero.setSaldo(juegoAhorcado.getSaldo());
-                    juegoAdivinarNumero.jugar();
-                    System.out.println("SALDO FINAL: " + juegoAdivinarNumero.getSaldo());
                     
-                    System.out.println("1. Volver a jugar");
-                    System.out.println("2. Volver al incio");
-                    System.out.println("Digite el numero de la opcion?");
+                    juegoAdivinarNumero.jugar();
+                    
+                    juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
+                    
+                    System.out.println("                                                     ");
+                    System.out.println(" --------------------------------------------------- ");
+                    System.out.println("|                                                   |");
+                    System.out.println("| SALDO FINAL: " + juegoAdivinarNumero.getSaldo());
+                    System.out.println(" --------------------------------------------------- ");
+                    
+                    while (juegoAdivinarNumero.getSaldo() == 0) {
+
+                        System.out.println("|                                                   |");
+                        System.out.println("| COMO SU SALDO AHORA ES: 0                         |");
+                        System.out.println("|                                                   |");
+                        System.out.println("| 1. Ingrese nuevo saldo si quiere volver a jugar   |");
+                        System.out.println("|                                                   |");
+                        System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
+                        int volver = scanner.nextInt();
+                        System.out.println(" --------------------------------------------------- ");
+                        
+                        if (volver == 1) {
+
+                            while (juegoAdivinarNumero.getSaldo() == 0) {
+
+                                System.out.println("|                                                   |");
+                                System.out.print  ("| INGRESE UN SALDO MAYOR A 0: ");
+                                juegoAdivinarNumero.setSaldo(scanner.nextInt());
+                                juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
+                                System.out.println(" --------------------------------------------------- ");
+                            }
+                        } else {
+
+                            break;
+                        }
+                    }
+                    
+                    System.out.println("|                                                   |");
+                    System.out.println("| 1. Volver a jugar                                 |");
+                    System.out.println("| 2. Volver al incio                                |");
+                    System.out.println("|                                                   |");
+                    System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
                     int volver = scanner.nextInt();
+                    System.out.println(" --------------------------------------------------- ");
+                    
+                    
                     if (volver == 1){
                         
-                        while(volver == 1){
-                            
+                        while (volver == 1) {
+
                             juegoAdivinarNumero.jugar();
-                            System.out.println("SALDO FINAL: " + juegoAdivinarNumero.getSaldo());
+                            System.out.println("                                                     ");
+                            System.out.println(" --------------------------------------------------- ");
+                            System.out.println("|                                                   |");
+                            System.out.println("| SALDO FINAL: " + juegoAdivinarNumero.getSaldo());
+                            System.out.println(" --------------------------------------------------- ");
+
+                            while (juegoAdivinarNumero.getSaldo() == 0) {
+
+                                System.out.println("|                                                   |");
+                                System.out.println("| COMO SU SALDO AHORA ES: 0                         |");
+                                System.out.println("|                                                   |");
+                                System.out.println("| 1. Ingrese nuevo saldo si quiere volver a jugar   |");
+                                System.out.println("|                                                   |");
+                                System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
+                                volver = scanner.nextInt();
+                                System.out.println(" --------------------------------------------------- ");
+                                
+                                if (volver == 1) {
+
+                                    while (juegoAdivinarNumero.getSaldo() == 0) {
+
+                                        System.out.println("|                                                   |");
+                                        System.out.println("| INGRESE UN SALDO MAYOR A 0: ");
+                                        juegoAdivinarNumero.setSaldo(scanner.nextInt());
+                                        juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
+                                        System.out.println(" --------------------------------------------------- ");
+                                    }
+                                } else {
+
+                                    break;
+                                }
+                            }
                             
-                            System.out.println("1. Volver a jugar");
-                            System.out.println("2. Volver al incio");
-                            System.out.println("Digite el numero de la opcion?");
+                            System.out.println("|                                                   |");
+                            System.out.println("| 1. Volver a jugar                                 |");
+                            System.out.println("| 2. Volver al incio                                |");
+                            System.out.println("|                                                   |");
+                            System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
                             volver = scanner.nextInt();
+                            System.out.println(" --------------------------------------------------- ");
                         }
                     }
                     
@@ -80,23 +165,95 @@ public class Main {
                     juegoAhorcado.setSaldo(juegoAdivinarNumero.getSaldo());
                   
                     juegoAhorcado.jugar();
-                    System.out.println("SALDO FINAL: " + juegoAhorcado.getSaldo());
                     
-                    System.out.println("1. Volver a jugar");
-                    System.out.println("2. Volver al incio");
-                    System.out.println("Digite el numero de la opcion?");
+                    juegoAdivinarNumero.setSaldo(juegoAhorcado.getSaldo());
+                    
+                    System.out.println("                                                     ");
+                    System.out.println(" --------------------------------------------------- ");
+                    System.out.println("|                                                   |");
+                    System.out.println("| SALDO FINAL: " + juegoAhorcado.getSaldo());
+                    System.out.println(" --------------------------------------------------- ");
+                    
+                    while (juegoAhorcado.getSaldo() == 0) {
+
+                        System.out.println("|                                                   |");
+                        System.out.println("| COMO SU SALDO AHORA ES: 0                         |");
+                        System.out.println("|                                                   |");
+                        System.out.println("| 1. Ingrese nuevo saldo si quiere volver a jugar   |");
+                        System.out.println("|                                                   |");
+                        System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
+                        volver = scanner.nextInt();
+                        System.out.println(" --------------------------------------------------- ");
+
+                        if (volver == 1) {
+
+                            while (juegoAhorcado.getSaldo() == 0) {
+
+                                System.out.println("|                                                   |");
+                                System.out.println("| INGRESE UN SALDO MAYOR A 0: ");
+                                juegoAhorcado.setSaldo(scanner.nextInt());
+                                juegoAdivinarNumero.setSaldo(juegoAhorcado.getSaldo());
+                                System.out.println(" --------------------------------------------------- ");
+                            }
+                        } else {
+
+                            break;
+                        }
+                    }
+                    
+                    System.out.println("|                                                   |");
+                    System.out.println("| 1. Volver a jugar                                 |");
+                    System.out.println("| 2. Volver al incio                                |");
+                    System.out.println("|                                                   |");
+                    System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
                     volver = scanner.nextInt();
+                    System.out.println(" --------------------------------------------------- ");
+                    
                     if (volver == 1){
                         
                         while(volver == 1){
                             
                             juegoAhorcado.jugar();
-                            System.out.println("SALDO FINAL: " + juegoAhorcado.getSaldo());
+                            System.out.println("                                                     ");
+                            System.out.println(" --------------------------------------------------- ");
+                            System.out.println("|                                                   |");
+                            System.out.println("| SALDO FINAL: " + juegoAhorcado.getSaldo());
+                            System.out.println(" --------------------------------------------------- ");
                             
-                            System.out.println("1. Volver a jugar");
-                            System.out.println("2. Volver al incio");
-                            System.out.println("Digite el numero de la opcion?");
+                            while (juegoAdivinarNumero.getSaldo() == 0) {
+
+                                System.out.println("|                                                   |");
+                                System.out.println("| COMO SU SALDO AHORA ES: 0                         |");
+                                System.out.println("|                                                   |");
+                                System.out.println("| 1. Ingrese nuevo saldo si quiere volver a jugar   |");
+                                System.out.println("|                                                   |");
+                                System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
+                                volver = scanner.nextInt();
+                                System.out.println(" --------------------------------------------------- ");
+                                
+                                if (volver == 1) {
+
+                                    while (juegoAhorcado.getSaldo() == 0) {
+
+                                        System.out.println("|                                                   |");
+                                        System.out.println("| INGRESE UN SALDO MAYOR A 0: ");
+                                        juegoAhorcado.setSaldo(scanner.nextInt());
+                                        juegoAdivinarNumero.setSaldo(juegoAhorcado.getSaldo());
+                                        System.out.println(" --------------------------------------------------- ");
+                                    }
+                                } else {
+
+                                    break;
+                                }
+                            }
+                            
+                            System.out.println("|                                                   |");
+                            System.out.println("| 1. Volver a jugar                                 |");
+                            System.out.println("| 2. Volver al incio                                |");
+                            System.out.println("|                                                   |");
+                            System.out.print  ("| DIGITE EL NUMERO DE LA OPCION: ");
                             volver = scanner.nextInt();
+                            System.out.println(" --------------------------------------------------- ");
                         }
                     }
                     
@@ -109,11 +266,20 @@ public class Main {
                     break;
                     
                 case 3:
-                    System.out.println("¡Gracias por jugar!");
+                    
+                    System.out.println("|                                                   |");
+                    System.out.println("| GRACIAS POR JUGAR!!! :)                           |");
+                    System.out.println(" --------------------------------------------------- ");
+
+                    
                     return;
                     
                 default:
-                    System.out.println("¡Opción inválida! Por favor, elige una opción válida.");
+                    System.out.println("|                                                   |");
+                    System.out.println("| OPCION INVALIDA!!!!                               |");
+                    System.out.println("| ELIJA UN OPCION VALIDA                            |");
+                    System.out.println(" --------------------------------------------------- ");
+
             }
         }
     }
