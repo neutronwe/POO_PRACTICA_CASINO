00762db36280f29b8065f104be4ab1bd3c07e2f5
Santiago Tellez <147094912+neutronwe@users.noreply.github.com>
2024-05-15 01:35:52 -0500

    Update JuegoAdivinarNumero.java

--- JuegoAdivinarNumero.java
+++ JuegoAdivinarNumero.java
@@ -22,22 +22,40 @@ public class JuegoAdivinarNumero extends Juegos{
     @Override
     protected void jugar() {
         
-        System.out.println("-------JUEGO ADIVINA EL NUMERO-------");
-        System.out.print("Introduce el valor que vas a apostar: ");
+        System.out.println("                                                     ");
+        System.out.println(" ------------JUEGO ADIVINA EL NUMERO----------------" );
+        System.out.println("|                                                   |");
+        System.out.print  ("| INGRESA EL VALOR PARA APOSTAR: ");
         valorApuesta = scanner.nextInt();
-        System.out.println("Estoy pensando en un número entre 1 y 100.");
-
+        System.out.println(" --------------------------------------------------- "); 
+        
+        System.out.println("                                                     ");
+        System.out.println(" --------------------------------------------------- ");  
+        System.out.println("|                                                   |");
+        System.out.println("| Estoy pensando en un numero entre 1 y 100         |");
+        
         while (true) {
-            System.out.print("Introduce tu adivinanza: ");
+            System.out.println("|                                                   |");
+            System.out.print  ("| INGRESA UN NUMERO: ");
             int adivinanza = scanner.nextInt();
+            System.out.println(" --------------------------------------------------- "); 
             intentos++;
 
             if (adivinanza == numeroAleatorio) {
-                System.out.println("¡Felicidades! ¡Has adivinado el número en " + intentos + " intentos!");
+                System.out.println("                                                     ");
+                System.out.println(" --------------------------------------------------- "); 
+                System.out.println("| Has adivinado el número en " + intentos + " intentos");
+                System.out.println("|                                                   |");
+                System.out.println("| Recuerda que mayor a 5 intentos pierdes           |");
+                System.out.println("| todo lo apostado de lo contrario ganas el doble   |");
+                System.out.println(" --------------------------------------------------- "); 
                 if (intentos >= 5){
                     
                     saldo -=  valorApuesta;
-                    System.out.println("PERDISTE: " + valorApuesta);
+                    System.out.println("                                                     ");
+                    System.out.println(" --------------------------------------------------- "); 
+                    System.out.println("| PERDISTE: " + valorApuesta);
+                    System.out.println(" --------------------------------------------------- "); 
                     //System.out.println("SALDO FINAL: " + saldo);
                 }
                 else {
@@ -45,15 +63,27 @@ public class JuegoAdivinarNumero extends Juegos{
                     if (intentos < 5){
                         
                         saldo += valorApuesta * 2;
+                        System.out.println("                                                     ");
+                        System.out.println(" --------------------------------------------------- "); 
                         System.out.println("GANASTE: " + valorApuesta*2);
-                        System.out.println("SALDO FINAL: " + saldo);
+                        System.out.println(" --------------------------------------------------- ");
+                        //System.out.println("SALDO FINAL: " + saldo);
                     }
                 }
                 break;
             } else if (adivinanza < numeroAleatorio) {
-                System.out.println("El número que estoy pensando es mayor que " + adivinanza + ".");
+                System.out.println("                                                     ");
+                System.out.println(" --------------------------------------------------- ");
+                System.out.println("|                                                   |");
+                System.out.println("| El numero que estoy pensando es mayor que " + adivinanza + ".");
+                System.out.println(" --------------------------------------------------- ");
+                
             } else {
-                System.out.println("El número que estoy pensando es menor que " + adivinanza + ".");
+                System.out.println("                                                     ");
+                System.out.println(" --------------------------------------------------- ");
+                System.out.println("|                                                   |");
+                System.out.println("| El numero que estoy pensando es menor que " + adivinanza + ".");
+                System.out.println(" --------------------------------------------------- ");
             }
         }
         intentos = 0;
